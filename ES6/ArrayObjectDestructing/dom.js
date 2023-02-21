1e9603ec61451519f4054fcc000f5292f0d7182f
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-21 18:59:08 +0600

    Add files via upload

---
+++ ES6/ArrayObjectDestructing/dom.js
@@ -0,0 +1,18 @@
+// object
+const student = {
+    names : 'Unknown',
+    age: 35,
+    profession: 'student',
+
+}
+const {age, profession, names} = student;
+console.log(age, profession, names);
+
+
+// array
+const [first, another] = [44, 99, 88, 66];
+console.log(first, another);
+
+const nam = ['Sojib', 'Rakib', 'Roni', 'Juwel'];
+const [firstName, secondName,thirdName] = nam;
+console.log(thirdName, firstName);
