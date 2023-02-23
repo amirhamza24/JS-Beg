17c1110ed25753251469a67a7475f974e1da1202
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-23 13:33:17 +0600

    Add files via upload

---
+++ ES6/Mod-32/Practice-Problem/Problem2FilterFind/practiceProblem.js
@@ -0,0 +1,10 @@
+// practice problem 2 ** filter **
+const filterNumbers = [33, 50, 79, 78, 90, 101, 30];
+const filterResult = filterNumbers.filter(filterNumbers => filterNumbers % 10 == 0);
+console.log(filterResult);
+
+
+// practice problem 3 ** find **
+const findNumbers = [33, 50, 79, 78, 90, 101, 30];
+const findResult = findNumbers.find(findNumbers => findNumbers % 10 == 0);
+console.log(findResult);
