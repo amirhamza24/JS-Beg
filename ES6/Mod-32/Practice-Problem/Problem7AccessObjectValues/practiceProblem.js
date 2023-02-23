898086745c8b204177c660c78ece09eb1a35144d
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-23 13:58:58 +0600

    Update practiceProblem.js

--- ES6/Mod-32/Practice-Problem/Problem7AccessObjectValues/practiceProblem.js
+++ ES6/Mod-32/Practice-Problem/Problem7AccessObjectValues/practiceProblem.js
@@ -1,29 +1,37 @@
-const student = {
-    name: 'Fredie',
-    age: 26,
+// access object values
+const user = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+        street: "Kulas Light",
+        suite: "Apt, 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+        }
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets"
+    }
 }
 
-const studentAgeDot = student.age;
-const studentAgeBracket = student['age'];
+const userEmail = user.email;
+const userAddress = user.address.street;
+const userCity = user.address.city;
+const userLat = user.address.geo.lat;
+const userCompany = user.company.name;
 
-console.log(studentAgeDot);
-console.log(studentAgeBracket);
+console.log(userEmail);
+console.log(userAddress);
+console.log(userCity);
+console.log(userLat);
+console.log(userCompany);
 
-
-// console the value of city
-let data = {
-    location: [
-        {
-            latitude: '34.5, 37.8',
-            longitude: '98.77, 58.7',
-            city: 'Hyderabad',
-            country: 'India',
-        }
-    ]
-};
-
-const dataCityDot = data.location[0].city;
-const dataCityBracket = data['location'][0]['city']
-
-console.log(dataCityDot);
-console.log(dataCityBracket);
