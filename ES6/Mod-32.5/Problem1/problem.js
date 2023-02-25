df9b2505cdb04d923d6abb6890f4d3d8fa96b5af
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-25 12:01:19 +0600

    Add files via upload

---
+++ ES6/Mod-32.5/Problem1/problem.js
@@ -0,0 +1,11 @@
+const name = "Shakib";
+const val = `Hello, ${name}`;
+
+console.log(val);
+
+const person = {
+    name: "Rakib",
+    age: 24,
+}
+
+console.log(`My name is ${person.name} and I am ${person.age} years old`);
