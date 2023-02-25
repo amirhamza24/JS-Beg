b59bd67ff32798da6c81595327f1f40c70d0b4bc
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-25 12:22:07 +0600

    Add files via upload

---
+++ ES6/Mod-32.5/Problem2.4/problem.js
@@ -0,0 +1,12 @@
+const sum = (num1, num2) => {
+    const sum1 = num1 + 2;
+    const sum2 = num2 + 2;
+
+    return sum1 * sum2;
+};
+
+const a = 3;
+const b = 4;
+
+const result = sum(a, b);
+console.log(result);
