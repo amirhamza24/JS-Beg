d9569d06dca50d68ec3315e932aaa6c7e9f81b6d
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-02-25 18:48:14 +0600

    Add files via upload

---
+++ ES6/Mod-33/DynamicallyAddedSectionWithCSS/post.js
@@ -0,0 +1,32 @@
+function loadPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+
+// get the container element where you want to add the new element
+// create child element
+// set innerText or inner HTML
+// appendChild
+
+
+function displayPosts(posts) {
+    const postContainer = document.getElementById('post-container');
+
+    for(const post of posts) {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        postDiv.innerHTML = `
+            <h4>User-${post.userId}</h4>
+            <h5>Post: ${post.title}</h5>
+            <p>Post Description: ${post.body}</p>
+        
+        
+        `;
+        postContainer.appendChild(postDiv);
+    }
+}
+
+loadPost();
