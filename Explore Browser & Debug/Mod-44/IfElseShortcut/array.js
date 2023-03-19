6a8cf2c9a16d44f75b72c9e05c0763237c3f0257
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-03-20 04:40:10 +0600

    Add files via upload

---
+++ Explore Browser & Debug/Mod-44/IfElseShortcut/array.js
@@ -0,0 +1,16 @@
+let money = 180;
+let food;
+
+if(money > 100) {
+    food = 'biriyani';
+}
+
+else {
+    food = 'cha biscuit';
+}
+
+// console.log(food);
+
+// if else shortcut using ternary operator
+let food1 = money > 100 ? 'Kacchi' : 'Tea';
+console.log(food1);
